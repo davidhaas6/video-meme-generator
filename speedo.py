75565328e39895232264329f1502eba80f4c1070
davidhaas6 <davidhaas6@gmail.com>
2018-01-06 12:03:48 -0500

    Added heuristic for phrase location

--- speedo.py
+++ speedo.py
@@ -1,6 +1,7 @@
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 import youtube_dl
 import pprint
+import os
 from datetime import datetime
 
 
@@ -9,35 +10,55 @@ from datetime import datetime
 # https://github.com/rg3/youtube-dl/blob/master/README.md#embedding-youtube-dl
 # https://zulko.github.io/moviepy/ref/videofx/moviepy.video.fx.all.speedx.html?highlight=speed
 
+# A logger object for youtube-dl
+class Logger(object):
+    def warning(self, msg):
+        if 'subtitles' in msg:
+            print 'Error: ' + msg
+            print 'Please choose a new video'
+            print 'Quitting'
+            exit(2)
+
+    def error(self, msg):
+        print 'error:' + msg
+
+    def debug(self, msg):
+        pass
+
 
 # Formats strings in a consistent manner
 def fmt(string):
-    exclude = ['.', ':', ';', '!', ',', '?']
-    return ''.join(ch for ch in string if ch not in exclude).lower().replace('\n', ' ')
+    exclude = ['.', ':', ';', '!', ',', '?', '"']
+    return ''.join(ch for ch in string if ch not in exclude).lower().replace('\n', ' ').strip()
 
+video_url = 'https://www.youtube.com/watch?v=Slpz0D35oRI'
+phrase = 'grow'
 
-def dl_hook(d):
-    print 'ASDFASDFASDF'
-    print d['filename']
-
-
-video_url = 'https://www.youtube.com/watch?v=79DijItQXMM'
-phrase = 'You\'re welcome'
 phrase = fmt(phrase)
+video_id = video_url[video_url.find('=')+1:]
+vid_extension = 'mp4'
+video_name = video_id + '.' + vid_extension
+subs_name = video_id+'.en.vtt'
+output_name = "output.mp4"
 
 # Downloads the video and subtitles
+print 'Downloading video...'
 ydl_opts = {'writesubtitles': True,
-            'outtmpl': 'video.%(ext)s',
-            'format_spec': 'mp4',
-            'format': 'mp4'}
+            'outtmpl': video_id+'.%(ext)s',
+            'format_spec': vid_extension,
+            'format': vid_extension,
+            'logger': Logger()}
 
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download([video_url])
 
-duration = VideoFileClip("video.mp4").duration
+print 'Finished downloading'
+
+duration = VideoFileClip(video_name).duration
 
 # Reads the subtitles into the string
-with open('video.en.vtt', 'r') as subs_file:
+
+with open(subs_name, 'r') as subs_file:
     subs_str = subs_file.readlines()[4:]
 
 # Splits the subtitles into a 2d array
@@ -59,39 +80,44 @@ for line in subs_str:
         key = (start_time, end_time)
         subs[(start_time, end_time)] = ''
     elif line != '\\n':
-        subs[key] += fmt(line)
+        subs[key] += line
+
+# Formats all of the lines to remove noise
+for key, val in subs.items():
+    subs[key] = fmt(val)
 
 pprint.pprint(subs)
 
 # Creates an array of how often the phrase occurs in a given time span
-# TODO: This only works for single words, fix for phrases
 instances = []
 for time, line in subs.items():
     # Gets the indices of occurrences
-    # indices = [i for i, s in enumerate(line.split()) if s == phrase]
     indices = []
-    for i in range(len(line) - len(phrase)):
+    for i in range(len(line) - len(phrase) + 1):
         if line[i:i+len(phrase)] == phrase:
             indices += [i]
 
-    # Adds the appropriate number of instances
-    instances.append((time, len(indices)))
+    # A rough heuristic to pin down how far into the phrase is said
+    for i in indices:
+        # The percentage of how far in the line the phrase occurs in
+        pos = round(i / float(len(line)), 1)
+
+        # The timestamp the phrase should occur at
+        start, end = time
+        time_pos = start + (end - start) * pos
+        instances.append(time_pos)
 
 # Sorts from smallest to biggest
-instances.sort(key=lambda tup: tup[0][0])
-
-# Adds the beginning and end of the clip since they won't have subtitles
-instances.insert(0, ((0, instances[0][0][0]), 0))
-instances.insert(len(instances), ((instances[len(instances)-1][0][1], duration), 0))
+instances.sort()
 
 print instances
 
 # Conjoins and speeds the clips
 # https://zulko.github.io/moviepy/getting_started/compositing.html
-speed_multiplier = 1.1
+speed_multiplier = 1.33
 clip_arr = []
 phrase_count = 0  # Number of times the phrase has been said so far
-clip = VideoFileClip("video.mp4")
+clip = VideoFileClip(video_name)
 
 for time_span, num_occur in instances:
     start, end = time_span
@@ -100,4 +126,4 @@ for time_span, num_occur in instances:
 
 # Combines and writes the video
 final_clip = concatenate_videoclips(clip_arr)
-final_clip.write_videofile("output.mp4", preset='ultrafast')
+final_clip.write_videofile(output_name, preset='superfast')
